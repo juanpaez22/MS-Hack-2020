194cba496fc8457fa1626ed6b044ec99014cd2d8
JP Nelson <t-jen@microsoft.com>
2020-07-28 11:30:23 -0400

    fix x axis times

--- dist/data-viz.js
+++ dist/data-viz.js
@@ -1,16 +1,16 @@
 import {getMoodData,appendMoodData} from "./storage_utils.js"
 
-function createGraph(entries){
-    console.log("creating graph", entries)
+function createGraph(data){
+    console.log("creating graph", data)
     // set the dimensions and margins of the graph
 
     // create svg element:
-    var svg = d3.select("#chart").append("svg").attr("width", 340).attr("height", 300)
+    var svg = d3.select("#chart").append("svg").attr("width", 400).attr("height", 300)
 
     // Create the scale
     var x = d3.scalePoint()
-    .domain(["Sunday", "Monday","Tuesday", "Wednesday", "Thursday", "Friday","Saturday"])         // This is what is written on the Axis: from 0 to 100
-    .range([10, 340]);       // This is where the axis is placed: from 100 px to 800px
+    .domain(data[1])         // This is what is written on the Axis: from 0 to 100
+    .range([25, 340]);       // This is where the axis is placed: from 100 px to 800px
 
     svg
     .append("g")
@@ -18,12 +18,12 @@ function createGraph(entries){
     .call(d3.axisBottom(x));
 
     var y = d3.scalePoint()
-    .domain(["Amazing", "Great","Not The Best"])         // This is what is written on the Axis: from 0 to 100
+    .domain(["OK", "Great","Amazing"])         // This is what is written on the Axis: from 0 to 100
     .range([190, 10]);       // This is where the axis is placed: from 100 px to 800px
 
     svg
     .append("g")
-    .attr("transform", "translate(300,0)")      // This controls the vertical position of the Axis
+    .attr("transform", "translate(350,0)")      // This controls the vertical position of the Axis
     .call(d3.axisRight(y));
 
     // prepare a helper function
@@ -33,7 +33,7 @@ function createGraph(entries){
 
     // Add the path using this helper function
     svg.append('path')
-    .attr('d', lineFunc(entries))
+    .attr('d', lineFunc(data[0]))
     .attr('stroke', 'black')
     .attr('fill', 'none');
 
@@ -50,32 +50,57 @@ function scaleRange(min,max,a,b,x){
 }
 
 function getXAxis(first,last){
-
+    const month_map = {
+        0:"Jan",
+        1:"Feb",
+        2:"Mar",
+        3:"Apr",
+        4:"May",
+        5:"June",
+        6:"July",
+        7:"Aug",
+        8:"Sept",
+        9:"Oct",
+        10:"Nov",
+        11:"Dec"
+    }
+    let axis = new Array();
     let first_d = new Date(first.timestamp)
     let last_d = new Date(last.timestamp)
 
+    if(first_d.getDate() == last_d.getDate()){
+        //set axis to local time
+        axis.push(first_d.toLocaleTimeString('en-US'))
+        axis.push(last_d.toLocaleTimeString('en-US'))
+    }
 
-
-
+    //set axis to month and day
+    else{
+        axis.push(month_map[first_d.getMonth()] + " " + first_d.getDate())
+        axis.push(month_map[last_d.getMonth()] + " " + last_d.getDate())
+    }
+    
+    console.log(axis)
+    return axis
 }
 function convertEntries(entries){
     let converted_entries = new Array();
 
-    console.log(entries)
     let min = Date.parse(entries[0].timestamp)
     let max = Date.parse(entries[entries.length - 1].timestamp)
 
-    let a = 10;
+    let a = 25;
     let b = 340;
 
     entries.forEach((entry) => {
 
-        let x = scaleRange(min,max,a,b,Date.parse(entry.timestamp)) + 10
+        let x = scaleRange(min,max,a,b,Date.parse(entry.timestamp)) + 25
         let y = scaleRange(0,10,0,200,(10-entry.val))
         converted_entries.push({x: x, y:y})
     });
     
-    return converted_entries
+    let axis = getXAxis(entries[0],entries[entries.length - 1])
+    return [converted_entries,axis]
 }
 
 
